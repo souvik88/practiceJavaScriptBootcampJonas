69122d868b0c50de300cd27b26e5ec27a5bd420c
Souvik Kundu <kivtech88@gmail.com>
2021-12-03 23:47:43 +0530

    Create variables.js

---
+++ section1/variables.js
@@ -0,0 +1,13 @@
+/*
+LECTURE: Values and Variables
+1. Declare variables called 'country', 'continent' and 'population' and
+assign their values according to your own country (population in millions)
+2. Log their values to the console
+*/
+
+var country, continent, population;
+country = "India";
+continent = "Asia";
+population = 10000;
+
+console.log(country, continient, population)
