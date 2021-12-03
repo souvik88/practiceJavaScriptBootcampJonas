d312282f37f9538a00a15913f2a02e93af58fc4c
Souvik Kundu <kivtech88@gmail.com>
2021-12-04 01:31:04 +0530

    Update dataTypes.js

--- section1/dataTypes.js
+++ section1/dataTypes.js
@@ -5,12 +5,25 @@ country. The variable should hold a Boolean value. Also declare a variable
 'language', but don't assign it any value yet
 2. Log the types of 'isIsland', 'population', 'country' and 'language'
 to the console
+
+let, const and var
+1. Set the value of 'language' to the language spoken where you live (some
+countries have multiple languages, but just choose one)
+2. Think about which variables should be const variables (which values will never
+change, and which might change?). Then, change these variables to const.
+3. Try to change one of the changed variables now, and observe what happens
 */
 
-let isIsland = false;
-let language;
+const isIsland = false;
+const language = 'English';
 let country = 'India';
 let population = 1000;
+let description = `${}`
 
 console.log(typeof isIsland, typeof population, typeof country, typeof language);
+
+console.log(isIsland);
+console.log(country);
+console.log(population);
+console.log(language);
 
