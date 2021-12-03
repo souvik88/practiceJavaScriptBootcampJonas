f1b49438878d91c5dabcd9c07aed7074130d82d1
Souvik Kundu <kivtech88@gmail.com>
2021-12-03 23:56:25 +0530

    commit

---
+++ section1/dataTypes.js
@@ -0,0 +1,15 @@
+/*
+Data Types
+1. Declare a variable called 'isIsland' and set its value according to your
+country. The variable should hold a Boolean value. Also declare a variable
+'language', but don't assign it any value yet
+2. Log the types of 'isIsland', 'population', 'country' and 'language'
+to the console
+*/
+
+let isIsland = false;
+let language = 'English';
+let country = 'India';
+let population = 1000;
+
+console.log(typeof isIsland, typeof population, typeof country, typeof language);
--- section1/variables.js
+++ section1/variables.js
@@ -1,5 +1,5 @@
 /*
-LECTURE: Values and Variables
+Values and Variables
 1. Declare variables called 'country', 'continent' and 'population' and
 assign their values according to your own country (population in millions)
 2. Log their values to the console
